8a3172fb82e429351e961f23528d696a56fedd42
Hamzeh Sabbagh <hamzasabbagh215@gmail.com>
2025-10-23 12:50:22 +0300

    new changes

--- main.py
+++ main.py
@@ -1,25 +1,47 @@
-from question_bank import question_bank as q_bank
+from quizbrain import QuizBrain
 
-questions = q_bank
 
-score = 0
-question_number = 1
+quizbrain = QuizBrain()
 
-for q in questions:
-    print(f"Question number:{question_number}\n")
-    print(q["question"])
-    for opt in q["options"]:
-        print(opt)
-    answer = input("Your answer (A/B/C/D): ").upper()
-    if answer == q["answer"]:
+
+
+while range(10):
+    score = 0
+    question_number = 1
+
+    while True:
+        difficulty = input("Welcome to hz-quiz-game you will be asked 10 questions "
+        "and you will choose these questions difficulties by typing e for easy, m for medium, h for hard: ").lower()
+        
+        if difficulty == 'e':
+            q, a_option, a = quizbrain.easy_question_call()
+            break
+        elif difficulty == 'm':
+            q, a_option, a = quizbrain.medium_question_call()
+            break
+        elif difficulty == 'h':
+            q, a_option, a = quizbrain.hard_question_call()
+            break
+        else:
+            print("wrong input please try again")
+                
+    print(f"Q{question_number}")
+    print(q)
+    print(a_option)
+    choice = input("Choose between A/B/C/D: ").upper()
+    if choice == a:
         print("Correct!\n")
         score += 1
+
     else:
-        print(f"Wrong! The answer is {q['answer']}.\n")
-
+        print(f"Wrong! The answer is {a}.\n")
+    
     question_number += 1
-    print(f"Your current score is: {score}/{question_number - 1}\n")
 
-print(f"Quiz finished! Your score: {score}/{len(questions)}")
+print(f"Quiz finished! Your score: {score}/10")
+
+
+
+
 
 # testing
